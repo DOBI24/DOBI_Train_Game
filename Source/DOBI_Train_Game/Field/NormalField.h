c326b491906abdaa2778479adc9befca44933d4f
Marton Pál <martonpal00@gmail.com>
2024-09-09 12:46:32 +0200

    Create Field classes

---
+++ Source/DOBI_Train_Game/Field/NormalField.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BaseField.h"
+#include "NormalField.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DOBI_TRAIN_GAME_API ANormalField : public ABaseField
+{
+	GENERATED_BODY()
+	
+};
