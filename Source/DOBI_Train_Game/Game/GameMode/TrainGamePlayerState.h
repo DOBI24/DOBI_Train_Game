e28d822373ecdf0117c9d7dee024460b171ab33c
Marton Pál <martonpal00@gmail.com>
2024-10-08 21:33:59 +0200

    Create PlayerState class

---
+++ Source/DOBI_Train_Game/Game/GameMode/TrainGamePlayerState.h
@@ -0,0 +1,17 @@
+// @Copyright Marton Pal SZTE
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "TrainGamePlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DOBI_TRAIN_GAME_API ATrainGamePlayerState : public APlayerState
+{
+	GENERATED_BODY()
+	
+};
