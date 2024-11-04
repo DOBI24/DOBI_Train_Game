1c0b357f81d276fd3af1806808b8016edfa1e8a1
Marton Pál <martonpal00@gmail.com>
2024-11-04 16:32:01 +0100

    Initialize constants

--- Source/DOBI_Train_Game/Game/GameMode/TrainGameMode.h
+++ Source/DOBI_Train_Game/Game/GameMode/TrainGameMode.h
@@ -15,5 +15,8 @@ class DOBI_TRAIN_GAME_API ATrainGameMode : public AGameMode
 	GENERATED_BODY()
 	
 public:
+	static const int32 MAX_TRAIN_COUNT = 45;
+	static const int32 MAX_STATION_COUNT = 3;
+
 	ATrainGameMode();
 };
