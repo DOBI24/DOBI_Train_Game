e68152487e91aca49c936e4ab9c585cad83339f2
Marton Pál <martonpal00@gmail.com>
2024-11-09 23:10:45 +0100

    Const TIMEs

--- Source/DOBI_Train_Game/Game/GameMode/TrainGameMode.h
+++ Source/DOBI_Train_Game/Game/GameMode/TrainGameMode.h
@@ -15,8 +15,13 @@ class DOBI_TRAIN_GAME_API ATrainGameMode : public AGameMode
 	GENERATED_BODY()
 	
 public:
+	/* PLAYER CONSTANTS */
 	static const int32 MAX_TRAIN_COUNT = 45;
 	static const int32 MAX_STATION_COUNT = 3;
+
+	/* TIMER CONSTANTS */
+	static const int32 DRAW_ROUTE_TIME = 30;
+	static const int32 GAME_TIME = 60;
 
 	ATrainGameMode();
 };
